096780836c266149cf4692f59c4c22fe89188b0c
Zorin <zorin1477@gmail.com>
2021-07-13 15:15:34 +0800

    chore: componentize

--- layouts/default.vue
+++ layouts/default.vue
@@ -33,15 +33,13 @@
       </v-list>
     </v-navigation-drawer>
     <v-app-bar dense fixed app elevate-on-scroll>
-      <v-app-bar-nav-icon id="appbaricon" @click.stop="drawer = !drawer" />
+      <v-app-bar-nav-icon id="appbar-icon" @click.stop="drawer = !drawer" />
       <v-toolbar-title v-text="title" />
       <v-spacer></v-spacer>
       <v-btn icon>
         <v-icon>mdi-file-tree</v-icon>
       </v-btn>
-      <v-btn icon @click="changeColorScheme">
-        <v-icon v-text="getSchemeIcon"></v-icon>
-      </v-btn>
+      <color-scheme-switch></color-scheme-switch>
       <template #extension>
         <v-app-bar-nav-icon style="visibility: hidden"></v-app-bar-nav-icon>
         <v-icon>mdi-folder</v-icon>
@@ -49,7 +47,7 @@
         <v-spacer></v-spacer>
         <path-edit></path-edit>
         <sort-menu></sort-menu>
-        <view-mode></view-mode>
+        <view-mode-switch></view-mode-switch>
       </template>
     </v-app-bar>
     <v-main>
@@ -75,23 +73,23 @@
 </template>
 
 <script lang="ts">
-import { Component, Watch } from 'vue-property-decorator'
+import { Component, Vue } from 'vue-property-decorator'
 import BreadCrumbsNav from '~/components/BreadCrumbsNav.vue'
 import SortMenu from '~/components/SortMenu.vue'
 import PathEdit from '~/components/PathEdit.vue'
-import ViewMode from '~/components/ViewMode.vue'
-import EpiphyllumStore from '~/epiphyllum/store'
-import { ColorScheme } from '~/epiphyllum/utils'
+import ViewModeSwitch from '~/components/ViewModeSwitch.vue'
+import ColorSchemeSwitch from '~/components/ColorSchemeSwitch.vue'
 
 @Component({
   components: {
     BreadCrumbsNav,
     SortMenu,
     PathEdit,
-    ViewMode,
+    ViewModeSwitch,
+    ColorSchemeSwitch,
   },
 })
-export default class DefaultLayout extends EpiphyllumStore {
+export default class DefaultLayout extends Vue {
   private drawer = false
   private items = [
     {
@@ -107,62 +105,6 @@ export default class DefaultLayout extends EpiphyllumStore {
     right: 'mdi-chevron-right',
   }
 
-  private colorSchemes = ['light', 'dark', 'system']
-
-  private get getSchemeIcon(): string {
-    if (this.preferColorScheme === 'system') {
-      return 'mdi-desktop-mac'
-    } else if (this.$vuetify.theme.dark) {
-      return 'mdi-weather-night'
-    }
-
-    return 'mdi-weather-sunny'
-  }
-
   private title = 'Epiphyllum'
-
-  private changeColorScheme(): void {
-    this.colorSchemes.push(this.colorSchemes.shift() as string)
-    this.$store.commit('epiphyllum/changeColorScheme', this.colorSchemes[0])
-  }
-
-  private getSystemColorScheme(): ColorScheme {
-    if (window.matchMedia('(prefers-color-scheme: dark)').matches) {
-      return 'dark'
-    } else {
-      return 'light'
-    }
-  }
-
-  private setColorScheme(scheme: ColorScheme): void {
-    if (scheme === 'system') {
-      this.$vuetify.theme.dark = this.getSystemColorScheme() === 'dark'
-    } else {
-      this.$vuetify.theme.dark = scheme === 'dark'
-    }
-  }
-
-  private mounted(): void {
-    const that = this
-    if (this.preferColorScheme === 'system') {
-      this.colorSchemes.unshift(this.colorSchemes.pop() as string)
-    } else if (this.preferColorScheme === 'dark') {
-      this.colorSchemes.push(this.colorSchemes.shift() as string)
-    }
-
-    this.setColorScheme(this.preferColorScheme)
-    window
-      .matchMedia('(prefers-color-scheme)')
-      .addEventListener('change', () => {
-        if (that.preferColorScheme === 'system') {
-          that.setColorScheme(that.preferColorScheme)
-        }
-      })
-  }
-
-  @Watch('preferColorScheme')
-  private onColorSchemeChanged(): void {
-    this.setColorScheme(this.preferColorScheme)
-  }
 }
 </script>
