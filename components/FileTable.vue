518159bdb3e9e41d4110464cb9f864d126c13191
Zorin <zorin1477@gmail.com>
2021-08-06 01:11:05 +0800

    test: file list(table view)

---
+++ components/FileTable.vue
@@ -0,0 +1,41 @@
+<template>
+  <div id="file-table">
+    <!--    <v-data-table :headers="headers" :items="items"></v-data-table>-->
+  </div>
+</template>
+
+<script lang="ts">
+import { Component } from 'vue-property-decorator'
+import { DataTableHeader } from 'vuetify'
+import EpiphyllumStore from '~/epiphyllum/store'
+
+// interface FileTableItem extends Record<'filename' | 'time' | 'size', string> {}
+
+@Component
+export default class FileTable extends EpiphyllumStore {
+  private headers: DataTableHeader[] = [
+    {
+      text: '文件名',
+      value: 'filename',
+      align: 'start',
+      sortable: false,
+    },
+    {
+      text: '日期',
+      value: 'time',
+      align: 'center',
+      sortable: true,
+    },
+    {
+      text: '大小',
+      value: 'size',
+      align: 'center',
+      sortable: true,
+    },
+  ]
+
+  // private get items(): FileTableItem[] {
+  //
+  // }
+}
+</script>
