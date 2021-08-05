d11e0dd1a4ea12742776146c53db4cc71b3fce0e
Zorin <zorin1477@gmail.com>
2021-08-06 01:02:34 +0800

    feat: file download(WIP)

--- components/FileListItem.vue
+++ components/FileListItem.vue
@@ -1,45 +1,34 @@
 <template>
   <div id="filelist-item">
-    <div v-if="currentDirMeta" id="items">
-      <div v-if="!handledList.length" id="empty-directory">
-        <v-list-item disabled>
-          <v-list-item-content>
-            <v-list-item-title>Info: Empty directory.</v-list-item-title>
-          </v-list-item-content>
-        </v-list-item>
-      </div>
-      <div v-else id="has-item">
-        <v-list-item
-          v-for="(item, index) in handledList"
-          :key="'filelist-item' + index"
-          @click.stop="handleClick(item)"
-        >
-          <v-list-item-icon>
-            <v-icon v-if="item.isDir" v-text="icons.folder"></v-icon>
-            <v-icon v-else v-text="icons.file"></v-icon>
-          </v-list-item-icon>
-          <v-list-item-content>
-            <v-list-item-title v-text="item.pathname"></v-list-item-title>
-          </v-list-item-content>
-          <v-list-item-action>
-            <v-list-item-action-text v-text="item.transformedTime">
-            </v-list-item-action-text>
-            <v-list-item-action-text
-              v-if="!item.isDir"
-              v-text="item.transformedSize"
-            ></v-list-item-action-text>
-            <v-list-item-action-text v-else>-</v-list-item-action-text>
-          </v-list-item-action>
-        </v-list-item>
-      </div>
-    </div>
-    <div v-else id="error-no-item">
+    <div v-if="!currentFileList.length" id="empty-dir">
       <v-list-item disabled>
         <v-list-item-content>
-          <v-list-item-title
-            >Error: No such file or directory.</v-list-item-title
-          >
+          <v-list-item-title>Info: Empty directory.</v-list-item-title>
         </v-list-item-content>
+      </v-list-item>
+    </div>
+    <div v-else id="item-list">
+      <v-list-item
+        v-for="(item, index) in currentFileList"
+        :key="'filelist-item' + index"
+        @click.stop="handleClick(item)"
+      >
+        <v-list-item-icon>
+          <v-icon v-if="item.isDir" v-text="icons.folder"></v-icon>
+          <v-icon v-else v-text="icons.file"></v-icon>
+        </v-list-item-icon>
+        <v-list-item-content>
+          <v-list-item-title v-text="item.pathname"></v-list-item-title>
+        </v-list-item-content>
+        <v-list-item-action>
+          <v-list-item-action-text v-text="item.transformedTime">
+          </v-list-item-action-text>
+          <v-list-item-action-text
+            v-if="!item.isDir"
+            v-text="item.transformedSize"
+          ></v-list-item-action-text>
+          <v-list-item-action-text v-else>-</v-list-item-action-text>
+        </v-list-item-action>
       </v-list-item>
     </div>
   </div>
@@ -48,7 +37,7 @@
 <script lang="ts">
 import { join } from 'path'
 import { Component } from 'vue-property-decorator'
-import { DirectoryMeta, FileMeta, FileMetaList } from '~/epiphyllum/utils'
+import { FileMeta } from '~/epiphyllum/utils'
 import EpiphyllumStore from '~/epiphyllum/store'
 
 @Component
@@ -58,59 +47,13 @@ export default class FileListItem extends EpiphyllumStore {
     file: 'mdi-file-cloud',
   }
 
-  private get handledList(): FileMetaList {
-    return this.folderList.concat(this.fileList)
-  }
-
-  private get currentDirMeta(): DirectoryMeta | undefined {
-    return this.directoryMap.get(this.$route.path)
-  }
-
-  private get folderList(): FileMetaList {
-    if (!this.currentDirMeta || this.viewMode === 'file') {
-      return []
-    }
-
-    return this.sortList(this.currentDirMeta.files.filter((val) => val.isDir))
-  }
-
-  private get fileList(): FileMetaList {
-    if (!this.currentDirMeta || this.viewMode === 'directory') {
-      return []
-    }
-
-    return this.sortList(this.currentDirMeta.files.filter((val) => !val.isDir))
-  }
-
-  private sortList(list: FileMetaList): FileMetaList {
-    switch (this.sortMode) {
-      case 'time-asc': {
-        return list.sort((a, b) => a.time - b.time)
-      }
-
-      case 'time-desc': {
-        return list.sort((a, b) => b.time - a.time)
-      }
-
-      case 'size-asc': {
-        return list.sort((a, b) => a.size - b.size)
-      }
-
-      case 'size-desc': {
-        return list.sort((a, b) => b.size - a.size)
-      }
-
-      default: {
-        return list
-      }
-    }
-  }
-
   private handleClick({ pathname, isDir }: FileMeta): void {
     if (isDir) {
-      this.$router.push(join(this.$route.path, pathname))
+      this.$router.push(join(this.currentPath, pathname))
     } else {
-      alert('Coming soon')
+      const hostClip = this.host.split('/')
+      hostClip[2] = join(hostClip[2], this.currentPath, pathname)
+      window.location.href = hostClip.join('/')
     }
   }
 }
