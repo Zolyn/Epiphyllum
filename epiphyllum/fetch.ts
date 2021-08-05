ac857a6b568f34ea2cb1e1355b55a2bf3f12db44
Zorin <zorin1477@gmail.com>
2021-08-06 01:06:52 +0800

    feat: new states

--- epiphyllum/fetch.ts
+++ epiphyllum/fetch.ts
@@ -4,21 +4,25 @@ import { EpiphyllumEntry } from '~/epiphyllum/main'
 import EpiphyllumStore from '~/epiphyllum/store'
 
 @Component({
-  async fetch({ store }) {
+  async fetch({ store, route }) {
     const isInitialized: boolean = store.state.epiphyllum.isInitialized
     if (isInitialized) {
       Logger.info('No more initialization.')
       return
     }
 
-    const [epiphyllumErr, result] = await awaitHelper(EpiphyllumEntry())
+    const [epiphyllumErr, val] = await awaitHelper(EpiphyllumEntry())
 
-    if (!result) {
+    if (!val) {
       Logger.err(epiphyllumErr)
       return
     }
 
+    const { host, result } = val
+
     store.commit('epiphyllum/setDirMap', result)
+    store.commit('epiphyllum/setHost', host)
+    store.commit('epiphyllum/updatePath', route.path)
     store.commit('epiphyllum/initialize')
   },
 })
